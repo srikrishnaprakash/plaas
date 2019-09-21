6b122265461786452d89120c591074f141ebf879
srikrishna prakash <srikrishna_prakash@yahoo.com>
2019-09-21 12:11:27 +0530

    Added

--- createpl.groovy
+++ createpl.groovy
@@ -1,6 +1,4 @@
 pipelineJob('P1DSJ') {
-    
-    git [branch:'develop', url:'git@github.com:srikrishnaprakash/pls.git']
     definition {
         cps {
                 script("readFileFromWorkspace('ci.groovy')")
