31f7f04ca6d56a95d4a7257fc559b7f0091b7fe2
srikrishna prakash <srikrishna_prakash@yahoo.com>
2019-10-02 12:11:08 +0530

    Exe extension added

--- scripts/dep.bat
+++ scripts/dep.bat
@@ -1,4 +1,4 @@
 copy appl\target\arapp.war C:\sres\apache-tomcat-8.5.32\webapps /Y
-catalina stop
+catalina.bat stop
 del C:\sres\apache-tomcat-8.5.32\webapps\arapp /Q
-catalina start 
+catalina.bat start 
